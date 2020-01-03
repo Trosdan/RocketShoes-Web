3d79f69e7d120c4d7f0e51ec86030ca245d21648
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-01-03 11:48:56 +0000

    Updates

---
+++ precache-manifest.90a2460e799f67f3254cc3738d3e13e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138c24a98dfa0ce0a842bbbe49dc015a",
+    "url": "/RocketShoes-Web/index.html"
+  },
+  {
+    "revision": "7b2f1cd7c592e6b2f35d",
+    "url": "/RocketShoes-Web/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "7b2f1cd7c592e6b2f35d",
+    "url": "/RocketShoes-Web/static/js/2.a6ac6907.chunk.js"
+  },
+  {
+    "revision": "cb74db89aa8479b38805",
+    "url": "/RocketShoes-Web/static/js/main.1d7cda87.chunk.js"
+  },
+  {
+    "revision": "0572f142d29b9ad805c1",
+    "url": "/RocketShoes-Web/static/js/runtime~main.934fb9f2.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/RocketShoes-Web/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "879557c5bdbbd3743a7680c651e31d08",
+    "url": "/RocketShoes-Web/static/media/logo.879557c5.svg"
+  }
+]);
